39279cc58e7a307fb9fb3e5f29d1160c48af467a
Sxlvador <u19239752@tuks.co.za>
2022-06-08 21:06:14 +0200

    Add project files.

---
+++ HW3/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using u19239752_HW03.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace u19239752_HW03.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        
+        [HttpGet]
+        public ActionResult Index()
+        {
+           
+            
+            return View();
+        }
+
+
+        // gets  the file 
+        [HttpPost]
+        public ActionResult Index(string FileType, HttpPostedFileBase file)
+        {                     
+            
+               
+            // does a check  file is upload
+            if (  file != null)
+            {
+                string extension = Path.GetExtension(file.FileName);
+                
+
+                
+                if( FileType == "Image")
+                {
+                    // Save Image 
+                    file.SaveAs(Path.Combine(HttpContext.Server.MapPath("~/Media/Images"), Path.GetFileName(file.FileName)));
+                    ViewBag.Message = "File uploaded";
+                } 
+                else if( FileType == "Video")
+                {
+                    // Save video 
+                    file.SaveAs(Path.Combine(HttpContext.Server.MapPath("~/Media/Videos"), Path.GetFileName(file.FileName)));
+                    ViewBag.Message = "File uploaded";
+                }
+                else if(FileType == "Document")
+                {
+                    // Save Document 
+                    file.SaveAs(Path.Combine(HttpContext.Server.MapPath("~/Media/Documents"), Path.GetFileName(file.FileName)));
+                    ViewBag.Message = "File uploaded";
+                }
+            }
+            else
+            {
+                ViewBag.Message = "Plese Select a file:";
+             
+            }
+            // redirect the user
+
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ HW3/Models/FileModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u19239752_HW03.Models
+{
+    public class FileModel
+    {
+        public string FileName { get; set; }
+       /* public FileModel( string FileName)
+        {
+            FileName = this.FileName;   
+        }*/
+    }
+}
